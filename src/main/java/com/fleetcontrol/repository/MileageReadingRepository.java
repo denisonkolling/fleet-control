e1f3bdcc04000b67ced040f7f04d364e8d35ccea
Denison Kolling <denisonkolling@gmail.com>
2023-09-11 17:58:02 -0300

    feat: create insert miles validation

--- src/main/java/com/fleetcontrol/repository/MileageReadingRepository.java
+++ src/main/java/com/fleetcontrol/repository/MileageReadingRepository.java
@@ -1,7 +1,20 @@
 package com.fleetcontrol.repository;
 
 import com.fleetcontrol.model.MileageReading;
+import com.fleetcontrol.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface MileageReadingRepository extends JpaRepository<MileageReading, Long> {
+    MileageReading findTopByVehicleOrderByReadingDateDesc(Vehicle vehicle);
+
+    @Query("SELECT mr FROM MileageReading mr WHERE mr.vehicle = :vehicle AND mr.readingDate < :readingDate ORDER BY mr.readingDate DESC")
+    List<MileageReading> findByVehicleAndReadingDateBeforeOrderByReadingDateDesc(
+            @Param("vehicle") Vehicle vehicle,
+            @Param("readingDate") LocalDateTime readingDate
+    );
 }
--- src/main/java/com/fleetcontrol/service/impl/MileageReadingServiceImpl.java
+++ src/main/java/com/fleetcontrol/service/impl/MileageReadingServiceImpl.java
@@ -6,8 +6,11 @@ import com.fleetcontrol.model.Vehicle;
 import com.fleetcontrol.repository.MileageReadingRepository;
 import com.fleetcontrol.repository.VehicleRepository;
 import com.fleetcontrol.service.MileageReadingService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MileageReadingServiceImpl implements MileageReadingService {
@@ -20,9 +23,19 @@ public class MileageReadingServiceImpl implements MileageReadingService {
 
     @Override
     public MileageReading createMileageReading(MileageReadingRequest form) {
-        MileageReading mileageReading = new MileageReading();
-        Vehicle vehicle = vehicleRepository.findById(form.getVehicleId()).get();
 
+        Vehicle vehicle = vehicleRepository.findById(form.getVehicleId()).orElseThrow(() -> new EntityNotFoundException("Vehicle not found."));
+
+        List<MileageReading> previousReadings = mileageReadingRepository.findByVehicleAndReadingDateBeforeOrderByReadingDateDesc(vehicle, form.getReadingDate());
+
+        if (!previousReadings.isEmpty()) {
+            MileageReading mostRecentReading = previousReadings.get(0);
+            if (form.getReadingMiles() <= mostRecentReading.getReadingMiles()) {
+                throw new IllegalArgumentException("Miles reading must be higher than last reading before date insert.");
+            }
+        }
+
+        MileageReading mileageReading = new MileageReading();
         mileageReading.setVehicle(vehicle);
         mileageReading.setReadingDate(form.getReadingDate());
         mileageReading.setReadingMiles(form.getReadingMiles());
